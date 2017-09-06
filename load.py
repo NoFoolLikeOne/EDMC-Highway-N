a2feb3a8d2a078b78f156f361569dfb77f528c99
NoFoolLikeOne <31373535+NoFoolLikeOne@users.noreply.github.com>
2017-09-06 17:17:30 +0100

    Extensive Rewrite
    When in the bubble it uses cached data so EDSM query is kept to a minimum.
    First pass uses cached data to pick a star. It then queries EDSM in a background process and updates the UI if a star is found nearer.
    Star name is coloured green when final decision on the closest star is made. 
    Clicking on name only returns the name

--- load.py
+++ load.py
@@ -8,42 +8,68 @@ import os
 import requests
 import json
 from  math import sqrt,pow
+import threading
+from time import sleep
+from urllib import quote_plus
 
 from config import applongname, appversion
 import myNotebook as nb
 from config import config
 
+
+
 window=tk.Tk()
 window.withdraw()
+
+
 
 this = sys.modules[__name__]
 this.s = None
 this.prep = {}
+
+this.frame = None
+threads=[]
+#this.edsm_session = None
+#this.edsm_data = None
+
 
 # Lets capture the plugin name we want the name - "EDMC -"
 myPlugin = os.path.basename(os.path.realpath(os.path.dirname(os.path.realpath(__file__)))).split('-')[1]
 
 print os.path.realpath(os.path.dirname(os.path.realpath(__file__)))+'/bubble.json'
 
+
+	
+	
+with open(os.path.realpath(os.path.dirname(os.path.realpath(__file__)))+'/userstars.json') as user_file: 
+	userstars = json.load(user_file)		
+	
 with open(os.path.realpath(os.path.dirname(os.path.realpath(__file__)))+'/bubble.json') as bubble_file: 
-	nstars = json.load(bubble_file)
+	bubble = json.load(bubble_file)	
+	
+def dumpUserStars():
+	with open(os.path.realpath(os.path.dirname(os.path.realpath(__file__)))+'/userstars.json', 'w') as outfile:  
+		 json.dump(userstars, outfile)	
+
 	
 
 def edsmGetSystem(system):
 	url = 'https://www.edsm.net/api-v1/system?systemName='+system+'&showCoordinates=1'		
 	print url
 	r = requests.get(url)
-	return r.json()
+	s =  r.json()
+	print s
+	return { "x": s["coords"]["x"], "y": s["coords"]["y"], "z": s["coords"]["z"], "name": s["name"] }
 		
 			
 
 def getDistance(x1,y1,z1,x2,y2,z2):
 	return round(sqrt(pow(float(x2)-float(x1),2)+pow(float(y2)-float(y1),2)+pow(float(z2)-float(z1),2)),2)
 
-def setDistances(x,y,z):
+# def setDistances(x,y,z):
 			
-	for i, newt in enumerate(nstars):
-		nstars[i]["distance"]=getDistance(x,y,z,newt["coords"]["x"],newt["coords"]["y"],newt["coords"]["z"])
+	# for i, newt in enumerate(nstars):
+		# nstars[i]["distance"]=getDistance(x,y,z,newt["coords"]["x"],newt["coords"]["y"],newt["coords"]["z"])
 		#print newt
 
 
@@ -52,13 +78,14 @@ def plugin_start():
 	Load Template plugin into EDMC
 	"""
 	this.Range = tk.StringVar(value=config.get("Range"))
-	this.nearest = tk.StringVar(value=config.get("Nearest"))
-	this.nextnearest = tk.StringVar(value=config.get("NextNearest"))
+	this.nearest = 'No available'
+	this.distance = 999999
+	
+	this.userstars=userstars
 	
 	print os.path.realpath(os.path.dirname(os.path.realpath(__file__)))
 	
-	with open(os.path.realpath(os.path.dirname(os.path.realpath(__file__)))+'/nstars.json', 'w') as outfile:  
-		json.dump(nstars, outfile)
+	
 		
 	print myPlugin + "Loaded!"
 	
@@ -84,7 +111,7 @@ def plugin_prefs(parent,cmdr,is_beta):
 def copy_text_to_clipboard(event):
 	
 	window.clipboard_clear()  # clear clipboard contents
-	window.clipboard_append(this.status['text'])  # append new value to clipb			
+	window.clipboard_append(this.nearest)  # append new value to clipb			
 	
 def plugin_app(parent):
 	label = tk.Label(parent, text= myPlugin + ":")
@@ -96,6 +123,11 @@ def plugin_app(parent):
 		label_text="Out of Range"
 		
 	this.status = tk.Label(parent, anchor=tk.W, text=label_text)
+	
+	this.lastsystem = {}
+	this.frame = tk.Frame(parent)
+	#this.frame.columnconfigure(3, weight=1)
+	this.frame.bind('<<NeutronData>>', edsm_data)
 	
 	# We should ideally make this conditional
 	this.status.bind("<Button-1>", copy_text_to_clipboard)  
@@ -110,48 +142,124 @@ def prefs_changed():
 	
 	this.status['text'] = "Prefs changed"
 	
-# def extract_distance(json):
-    # try:
-        # Also convert to int since update_time will be string.  When comparing
-        # strings, "10" is smaller than "2".
-        # return int(json['primaryStar']['distance'])
-    # except KeyError:
-        # return 0
-		
-def startJumpEvent(system):
-	print "StartJump"
-	# get the coordinates of the system
-	currentSystem=edsmGetSystem(system)
-	print "currentSystem"
-	print currentSystem
-	try:
-		setDistances(system["x"],system["y"],system["z"])
-		this.jump_distance = getDistance(this.x,this.y,this.z,system["x"],system["y"],system["z"])
-		### we need to find the nearest
-		print "startJump found system"	
-		return True
-	except:
-		print "startJump not found system"	
-		return False
 	
 def fsdJumpEvent(system):
 	print "FSDJump"
 	#until getCoords(system)
 	#  findNearest(system,coords)
 		
-def getNearest():
+def findNearest(jumpsystem,list):
 	nearest	= { 'distance': 999999, 'name': "Out of Range" } 
 	n=999999
-	for sysrec in nstars:
+	for sysrec in list:
 		#print str(n) +  ">"  + str(sysrec['distance'])
-		if float(n) >  float(sysrec['distance']):
+		d = getDistance(jumpsystem["x"],jumpsystem["y"],jumpsystem["z"],sysrec["x"],sysrec["y"],sysrec["z"])
+		if float(n) > float(d):
 			try:
-				n = float(sysrec['distance'])
+				n = d
 				nearest=sysrec
 			except:
 				print exception
-	return nearest
+	return nearest["name"],n
 		
+def inBubble(s):
+	return getDistance(0,0,0,s["x"],s["y"],s["z"]) <= 800
+	#return False
+		
+def inColonia(s):
+	#{"name":"Colonia","coords":{"x":-9530.5,"y":-910.28125,"z":19808.125}}
+	return getDistance(-9530.5,-910.28125,19808.125,s["x"],s["y"],s["z"]) <= 200
+		
+def edsm_worker(system):
+	print "in edsm_worker going to sleep"
+	
+	print this.jumpsystem
+	jumpSystem=quote_plus(this.jumpsystem["name"])
+	jumpSystem="Colonia"
+	url = 'https://www.edsm.net/api-v1/sphere-systems?systemName='+jumpSystem+'&minRadius=0&radius=200&showPrimaryStar=1&filterPrimaryStar=N&showCoordinates=1'
+	print url
+	r = requests.get(url)
+	l = {}
+	for elem in userstars:
+		name=elem["name"]
+		x=elem["x"]
+		y=elem["y"]
+		z=elem["z"]
+		l[name]={ "x": x, "y": y,"z": z} 
+		
+	for elem in r.json():
+		name=elem["name"]
+		x=elem["coords"]["x"]
+		y=elem["coords"]["y"]
+		z=elem["coords"]["z"]
+		l[name]={ "x": x, "y": y,"z": z} 
+		
+	#n = []
+	del userstars[:]
+	
+	for key, value in l.iteritems():
+		#n.append({ "name": key, "x": value["x"],"y": value["y"],"z": value["z"]})
+		userstars.append({ "name": key, "x": value["x"],"y": value["y"],"z": value["z"]})
+		
+	this.nearest,this.distance = findNearest(this.jumpsystem,userstars)
+	
+	dumpUserStars()
+	
+	this.frame.event_generate('<<NeutronData>>', when='tail')
+	return
+		
+def edsm_data(event):
+	print "Waking up"
+	print this.nearest + " " + str(this.distance)
+	if this.distance == 999999:
+		this.status['text'] = this.nearest
+	else:
+		this.status['text'] = this.nearest+' ('+str(this.distance)+'ly)'				
+		this.status["fg"] = "green"
+	return
+		
+def initThread(jumpSystem):		
+	thread = threading.Thread(target = edsm_worker, args = (jumpSystem,))
+	##threads.append(thread)
+	thread.daemon = True
+	thread.start()
+		
+def setNearest(start,end=None):
+	print "setNearest"
+	print start
+	if not end == None:
+		print end
+		jump = getDistance(start["x"],start["y"],start["z"],end["x"],end["y"],end["z"])
+		jumpSystem=end
+	else:
+		print "No jump"
+		jumpSystem=start
+		
+	if inBubble(jumpSystem):
+		print "Near Sol"
+		this.nearest,this.distance = findNearest(jumpSystem,bubble)
+			
+	if inColonia(jumpSystem):
+		print "Near Colonia"
+		
+	if not inBubble(jumpSystem):
+		print "Not in bubble or colonia"
+		#we will display something right away
+		#and then send of a background query to EDSM
+		this.nearest,this.distance = findNearest(jumpSystem,userstars)
+		this.jumpsystem=jumpSystem
+		initThread(jumpSystem)
+		
+	print this.nearest + " " + str(this.distance)
+	if this.distance == 999999:
+		this.status['text'] = this.nearest
+	else:
+		this.status['text'] = this.nearest+' ('+str(this.distance)+'ly)'
+		
+	if not inColonia(jumpSystem) and not inBubble(jumpSystem):
+		this.status["fg"] = "grey"
+	else: 	
+		this.status["fg"] = "green"		
 # Detect journal events
 def journal_entry(cmdr, is_beta, system, station, entry, state):
 
@@ -159,69 +267,54 @@ def journal_entry(cmdr, is_beta, system, station, entry, state):
 	
 	if entry['event'] == 'StartUp':
 		
-		startSystem = edsmGetSystem(system)
-		print startSystem
-		setDistances(startSystem["coords"]["x"],startSystem["coords"]["y"],startSystem["coords"]["z"])
-		nearest = getNearest()
-		print nearest
-		if nearest['distance'] == 999999:
-			this.status['text'] = nearest['name']
-		else:
-			this.status['text'] = nearest['name']+' ('+str(nearest['distance'])+'ly)'
+		#We need the coords of the current system
+		#What if EDSM doesn't know? unlikely but could happen
+		this.lastsystem = edsmGetSystem(system)	
+		setNearest(this.lastsystem)
+
 	
 	if entry['event'] == 'Location':
 		print "Location"
-		setDistances(entry["StarPos"][0],entry["StarPos"][2],entry["StarPos"][2])
-		this.x = entry["StarPos"][0]
-		this.y = entry["StarPos"][1]
-		this.z = entry["StarPos"][2]
+		print entry
+		this.lastsystem = { "x": entry["StarPos"][0], "y": entry["StarPos"][1], "z": entry["StarPos"][2], "name": entry["StarSystem"] }
+		setNearest(this.lastsystem)
 
 	if entry['event'] == 'StartJump' and entry['JumpType'] == 'Hyperspace':
 
-		this.found = startJumpEvent(system) 
+		#We can assume that we know the location of the current system
+		# but what if we don't
+		
+		print "StartJump"
+		try:
+			##
+			this.jumpsystem = edsmGetSystem(system)	
+			this.foundsystem = True
+			setNearest(this.lastsystem,this.jumpSystem)
+		except:
+			print "Failed to find system"
+			this.foundsystem = False
 		
 	if entry['event'] == 'FSDJump':
+		#We can assume that we know the location of the current system
+		# but what if we don't
 	
-		if not this.found:
+		print "FSDJump"
+		print entry
+		if this.foundsystem:
+			print "Already calculated nearest"
+		else:
+			this.jumpsystem = { "x": entry["StarPos"][0], "y": entry["StarPos"][1], "z": entry["StarPos"][2], "name": entry["StarSystem"] }
+			setNearest(this.lastsystem,this.jumpsystem)
 			
-		
-			this.status['text'] = 'Finding nearest neutron star...'	
-			print system
-			radius = tk.StringVar(value=config.get("Range")).get()		
-			url = 'https://www.edsm.net/api-v1/sphere-systems?systemName='+system+'&minRadius=0&radius='+radius+'&showPrimaryStar=1&filterPrimaryStar=N'		
-			print url
-			r = requests.get(url)
-			print r.status_code
-			
-			#print r.json()
-			
-			input_dict = r.json()
-			output_dict = [x for x in input_dict if x['primaryStar'] and x['primaryStar']['type'] == 'Neutron Star' ]
-			
-			print output_dict
-			nearest	= { 'distance': 999, 'name': "Out of Range" } 
-			nextnearest	= { 'distance': 999, 'name': "Out of Range" } 
-			## what if there isnt any?
-			for sysrec in output_dict:
-				if float(nearest['distance']) >  sysrec['distance']:
-					try:
-						nextnearest=nearest
-						nearest=sysrec
-					except:
-						print exception
-			
-			print nearest
-			print nextnearest
-			
-			config.set("Nearest", nearest)
-			config.set("NextNearest", nextnearest)
-			this.status['text'] = nearest['name']+' ('+str(nearest['distance'])+'ly) '
+	
+	
 
 # Update some data here too
 def cmdr_data(data):
 	print "Commander Data"
-	print data
-	# this.status['text'] = "Commander Data"
+	this.lastsystem = edsmGetSystem(data['lastSystem']['name'])
+	setNearest(this.lastsystem)
+	
 
 # Defines location (system)
 def setLocation(location):
